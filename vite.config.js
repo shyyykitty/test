74136c363a03ddd57a6e1a04f06ddfd45d83d474
shyyykitty <none>
2024-02-03 18:45:09 -0500

    Fix deploy

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,7 @@ export default defineConfig({
             "@": fileURLToPath(new URL("./src", import.meta.url))
         }
     },
-    base: ""
+    base: process.env.NODE_ENV === 'production'
+        ? "/test/"
+        : "/"
 })
